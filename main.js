6c122aad55cf967e55da81d7a7b98a1d731ad462
Eric Julander <89485788+ejuland@users.noreply.github.com>
2021-10-08 11:24:20 -0600

    Added code to separate titles, suffexes, and to properly format phone numbers

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ function sendUsersToHCP(user_data) {
         "outputtype": "Json",
         "users": user_data
     };
-
     return axios.post(HCP_USER_ENDPOINT, JSON.stringify(body));
 }
 
@@ -39,20 +38,46 @@ function sendUsersToHCP(user_data) {
 function filterTitles(name) {
     return !name.match(/(Dr|Esq|Hon|Jr|Mrs|Mr|Msgr|Ms|Messrs|Mmes|Prof|Rev|Rt|Sr|St)(\.)*/g)
 }
+/**
+ * Determines if the provided string is a suffix or not
+ */
+function isSuffix(name) {
+    return name.match(/((jr\.?)|(sr\.?))$|(^(I+)|^(V+)|^(VI+)$)/img);
+}
+
+/**
+ * Takes the user phone number and coverts it into a ten-digit format for the server
+ */
+function formatPoneNumber(phone) {
+    //remove the counrty code from the front
+    let formatted_phone =
+        phone.replace(/^1-/m, "")
+        //remove all non-didgits
+        .replace(/\D/g, "")
+        //take the first ten numbers
+        .substring(0, 10)
+
+    return formatted_phone
+}
+
 
 /**
  * Takes a template user and transfoms it into the HCP format
  */
 function formatUser(user) {
     let names = user.name.split(/\s/).filter(filterTitles);
+    let first = names.shift();
+    while (isSuffix(names[names.length - 1])) {
+        names.pop();
+    }
     return {
 
-        "first_name": names.shift(),
+        "first_name": first,
         "last_name": names.pop(),
         "company_name": user.company.name,
         "company_full_address": `${user.address.street}, ${user.address.city}, ${user.address.zipcode}`,
         "website": user.website,
-        "phone": user.phone,
+        "phone": formatPoneNumber(user.phone),
 
     }
 }
@@ -76,6 +101,16 @@ async function handleHTTPError(res) {
 }
 
 /**
+ * Checks to see if the data successfully submitted based on the return response from the server 
+ */
+function checkSuccess(res) {
+    if (res.status == 200)
+        console.log("Data Successfully Submitted!")
+    else
+        return Promise.reject(res);
+}
+
+/**
  * Takes a set of template users from the jsonplaceholder database, 
  * transforms it into the HCP format, 
  * and then puts it into the HCP server
@@ -85,6 +120,7 @@ async function transferTemplateUsersToHCP() {
     getTemplateUsers()
         .then(usersToHCPFormat)
         .then(sendUsersToHCP)
+        .then(checkSuccess)
         .catch(handleHTTPError)
 }
 
